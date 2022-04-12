406151f6ff0c1a75444f012af9b9d47212404dd4
EC2 Default User <ec2-user@ip-172-31-28-251.ec2.internal>
2022-04-12 08:48:09 +0000

    [Add]books/showのレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,12 +12,23 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   def index
     @book = Book.new
     @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   private
