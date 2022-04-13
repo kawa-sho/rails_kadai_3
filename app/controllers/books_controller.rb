1a53785a6f35ad1a5912f3337a87752efc2ef612
EC2 Default User <ec2-user@ip-172-31-28-251.ec2.internal>
2022-04-13 11:48:46 +0000

    [Add]レイアウトと機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,30 @@
 class BooksController < ApplicationController
 
   def create
-    @book = Book.new(book_paramss)
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] ="You have created book successfully."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
+      @user = current_user
       render :index
     end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] ="You have update book successfully."
+      redirect_to book_path(params[:id])
+    else
+      render:edit
+    end
   end
 
   def destroy
@@ -29,13 +40,14 @@ class BooksController < ApplicationController
   end
 
   def show
+    @booknew = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
   end
 
   private
 
-  def book_paramss
+  def book_params
     params.require(:book).permit(:title, :body)
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
   get "/home/about" => "homes#about", as: "about"
 
-  resources :books, only: [:new, :index, :show, :create, :edit, :destroy]
+  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy]
 
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
